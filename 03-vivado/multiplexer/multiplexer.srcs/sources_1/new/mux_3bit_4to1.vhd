2eb042b54cc312fd0dcf971d5b93b382de07bb6f
Tomaskopriva <124770914+Tomaskopriva@users.noreply.github.com>
2023-03-01 23:27:38 +0100

    Update mux_3bit_4to1.vhd

--- 03-vivado/multiplexer/multiplexer.srcs/sources_1/new/mux_3bit_4to1.vhd
+++ 03-vivado/multiplexer/multiplexer.srcs/sources_1/new/mux_3bit_4to1.vhd
@@ -1,43 +1,23 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/22/2023 10:22:07 AM
--- Design Name: 
--- Module Name: mux_3bit_4to1 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity mux_3bit_4to1 is
---  Port ( );
-end mux_3bit_4to1;
+    port(
+        sel_i         : in  std_logic_vector(2 - 1 downto 0);
+        a_i           : in  std_logic_vector(3 - 1 downto 0);
+        b_i           : in  std_logic_vector(3 - 1 downto 0);
+        c_i           : in  std_logic_vector(3 - 1 downto 0);
+        d_i           : in  std_logic_vector(3 - 1 downto 0);
+        y_o           : out std_logic_vector(3 - 1 downto 0)
+    );
+end entity mux_3bit_4to1;
+
 
 architecture Behavioral of mux_3bit_4to1 is
-
 begin
+    y_o <= a_i when (sel_i = "00") else
+           b_i when (sel_i = "01") else
+           c_i when (sel_i = "10") else
+           d_i when (sel_i = "11");    
 
-
-end Behavioral;
+end architecture Behavioral;
