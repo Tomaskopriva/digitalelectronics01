d73800793d350abccd5b34b095ffefde26dcb700
Tomaskopriva <124770914+Tomaskopriva@users.noreply.github.com>
2023-05-02 23:19:56 +0200

    Update tb.vhd

--- Project/decoder.srcs/sim_1/new/tb.vhd
+++ Project/decoder.srcs/sim_1/new/tb.vhd
@@ -10,39 +10,26 @@ begin
     uut : entity work.morse_decoder
         port map (
             signal_in => signal_in
-
         );
 
     stim_proc : process
     begin
-
-
-        signal_in <= '0';
         wait for 3000 ms;
-
         signal_in <= '1';
         wait for 2000 ms;
-
         signal_in <= '0';
         wait for 6000 ms;
-
         signal_in <= '1';
         wait for 2000 ms;
-
         signal_in <= '0';
         wait for 6000 ms;
-
         signal_in <= '1';
         wait for 4000 ms;
-        
         signal_in <= '0';
         wait for 6000 ms;
-        
         signal_in <= '1';
         wait for 2000 ms;
-        
         signal_in <= '0';
-
         wait;
     end process;
 end architecture testbench;
